87b6c47ff65ba610304156d7f9f8f78d20cfcdbf
Nitay Caspi <NitayCaspi12@gmail.com>
2024-05-09 12:38:53 +0300

    create routes

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
 
+app.get("/users", (req, res) => {
+  res.send("Return users list");
+});
+
+app.get("/users/:id", (req, res) => {
+  let id = req.params.id;
+  res.send(`Return user id ${id} details`);
+});
+app.put("/users/:id", (req, res) => {
+  let id = req.params.id;
+  res.send(`Update user id ${id} details`);
+});
+app.delete("/users/:id", (req, res) => {
+  let id = req.params.id;
+  res.send(`Delete user id ${id}`);
+});
+app.post("/users", (req, res) => {
+  res.send("Add a user to the DB");
+});
+
 app.listen(3000, () => {
   console.log("server is up and running on port 3000");
 });
